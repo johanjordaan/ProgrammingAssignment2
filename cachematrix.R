faf362bb43c81ae4642370fe8a210b33ed1b56fd
Johan Jordaan <djjordaan@gmail.com>
2016-01-20 22:56:39 +0200

    Added some comments as required

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allows for the caching of the inverse of a matrix 
+## ** test() for usage examples
+##  
 
-## Write a short comment describing this function
 
+## Creates a cache matrix that can cache the inverse of a matrix
+## Retuns a 'object' with the the accessor methods and the inverse
+##  and target matrix 'closed' in
+## Expects a standard matrix as input (if none a defualr empty 1x1 matrix is created)
+## 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -18,17 +23,42 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Calculates the inverse of a matrix using a cache matrix 
+## Returs the inverse (calculated using solve)
+## Expects a cacheMatrix constraucte via makeCacheMatrix
+## * Passses all extra parameters to solve
+##
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   i <- x$getinverse()
   if(!is.null(i)) {
-    message("getting cached data")
+    #message("getting cached data")
     return(i)
   }
   data <- x$get()
   i <- solve(data, ...)
   x$setinverse(i)
   i
+}
+
+# This is a simple test of the functionality of this module
+#
+test <- function() {
+  # Create a random 3x3 matrix
+  m = matrix(rexp(9, rate=.1), ncol=3) 
+  
+  # Create the cached version
+  cm <- makeCacheMatrix(m)
+  if(sum(cm$get() == m) != length(m))  { message("m not set properly"); return }
+  if(!is.null(cm$getinverse())) { message("i not set properly"); return }
+    
+  # Calculate the inverse
+  im <- cacheSolve(cm)
+
+  # See if m multiplied with the Inverse give I 
+  if(sum(round(im %*% m) == diag(dim(m)[1])) != length(m)) message("inverse not calculated correctly")
+
+  # Check that the cached inverse is set  
+  if(is.null(cm$getinverse())) { message("i not set properly after solving"); return }
+  
 }
